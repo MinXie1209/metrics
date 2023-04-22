e767cd4083d8ebebc56b705caba3e21685846b6a
MinXie <xieyoujun@imshine.com>
2023-04-22 17:32:58 +0800

    feat(dubbo线程池命名取自threadFactory):

--- core/src/main/java/minxie/space/metrics/core/thread/ThreadPoolContext.java
+++ core/src/main/java/minxie/space/metrics/core/thread/ThreadPoolContext.java
@@ -1,12 +1,17 @@
 package minxie.space.metrics.core.thread;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ThreadPoolContext {
     private static final Set<Object> tomcatThreadPoolSet = new HashSet<Object>();
     private static final Set<Object> jdkThreadPoolSet = new HashSet<Object>();
-    private static final Set<Object> dubboThreadPoolSet = new HashSet<Object>();
+    // 加个名称，方便区分
+    private static final HashMap<Object, String> dubboThreadPoolMap = new HashMap<>();
+
+    private static final Set<String> threadPoolNameSet = new HashSet<>();
 
     public static Set<Object> getTomcatThreadPoolSet() {
         return tomcatThreadPoolSet;
@@ -26,13 +31,50 @@ public class ThreadPoolContext {
         jdkThreadPoolSet.add(obj);
     }
 
-    public static Set<Object> getDubboThreadPoolSet() {
-        return dubboThreadPoolSet;
+    public static HashMap<Object, String> getDubboThreadPoolMap() {
+        return dubboThreadPoolMap;
     }
 
     public static void addDubboThreadPool(Object obj) {
         System.out.println("addDubboThreadPool: " + obj);
         jdkThreadPoolSet.remove(obj);
-        dubboThreadPoolSet.add(obj);
+        String threadPoolName = getThreadPoolName(obj);
+        // 同名的话 加_index
+        boolean contains = !threadPoolNameSet.add(threadPoolName);
+        if (contains) {
+            int index = 1;
+            while (contains) {
+                contains = !threadPoolNameSet.add(threadPoolName + "_" + index);
+                if (!contains) {
+                    dubboThreadPoolMap.put(obj, threadPoolName + "_" + index);
+                }
+                index++;
+            }
+        } else {
+            dubboThreadPoolMap.put(obj, threadPoolName);
+        }
+
+    }
+
+    /**
+     * 获取线程池名称
+     * 从类的字段 threadFactory 获取父类NamedThreadFactory 获取他的字段值mPrefix
+     *
+     * @param obj
+     * @return
+     */
+    private static String getThreadPoolName(Object obj) {
+        try {
+            Field threadFactoryField = obj.getClass().getDeclaredField("threadFactory");
+            threadFactoryField.setAccessible(true);
+            Object threadFactory = threadFactoryField.get(obj);
+            Field mPrefixField = threadFactory.getClass().getSuperclass().getDeclaredField("mPrefix");
+            mPrefixField.setAccessible(true);
+            String mPrefix = (String) mPrefixField.get(threadFactory);
+            return mPrefix.substring(0, mPrefix.indexOf("-"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return obj.getClass().getSimpleName() + "@" + Integer.toHexString(obj.hashCode());
+        }
     }
 }
--- thread-pool-metrics/src/main/kotlin/minxie/space/thread/metrics/DubboThreadPoolMetricsVo.kt
+++ thread-pool-metrics/src/main/kotlin/minxie/space/thread/metrics/DubboThreadPoolMetricsVo.kt
@@ -16,30 +16,39 @@ private const val DUBBO_THREAD_POOL_QUEUE_SIZE = "dubbo_thread_pool_queue_size"
 
 private const val DUBBO_THREAD_POOL_COMPLETED_TASK_COUNT = "dubbo_thread_pool_completed_task_count"
 
+private const val HASH_CODE = "hashCode"
+
 class DubboThreadPoolMetricsVo : MetricBaseVo() {
     init {
-        ThreadPoolContext.getDubboThreadPoolSet().filterIsInstance<ThreadPoolExecutor>().forEach {
-            val threadPoolName = it.javaClass.simpleName + "@" + Integer.toHexString(it.hashCode())
-            val tag = mapOf(THREAD_POOL_NAME to threadPoolName)
-            it.activeCount?.let { activeCount ->
-                metricList.add(MetricItemVo(DUBBO_THREAD_POOL_ACTIVE_COUNT, activeCount.toFloat(), tag))
-            }
+        ThreadPoolContext.getDubboThreadPoolMap().filter { it.key is ThreadPoolExecutor }
+            .forEach { (it, threadPoolName) ->
+                it as ThreadPoolExecutor
+                val tag = mapOf(THREAD_POOL_NAME to threadPoolName, HASH_CODE to it.hashCode().toString())
+                it.activeCount.let { activeCount ->
+                    metricList.add(MetricItemVo(DUBBO_THREAD_POOL_ACTIVE_COUNT, activeCount.toFloat(), tag))
+                }
 
-            it.corePoolSize?.let { corePoolSize ->
-                metricList.add(MetricItemVo(DUBBO_THREAD_POOL_CORE_POOL_SIZE, corePoolSize.toFloat(), tag))
-            }
+                it.corePoolSize.let { corePoolSize ->
+                    metricList.add(MetricItemVo(DUBBO_THREAD_POOL_CORE_POOL_SIZE, corePoolSize.toFloat(), tag))
+                }
 
-            it.maximumPoolSize?.let { maximumPoolSize ->
-                metricList.add(MetricItemVo(DUBBO_THREAD_POOL_MAXIMUM_POOL_SIZE, maximumPoolSize.toFloat(), tag))
-            }
+                it.maximumPoolSize.let { maximumPoolSize ->
+                    metricList.add(MetricItemVo(DUBBO_THREAD_POOL_MAXIMUM_POOL_SIZE, maximumPoolSize.toFloat(), tag))
+                }
 
-            it.queue?.let { queue ->
-                metricList.add(MetricItemVo(DUBBO_THREAD_POOL_QUEUE_SIZE, queue.size.toFloat(), tag))
-            }
+                it.queue?.let { queue ->
+                    metricList.add(MetricItemVo(DUBBO_THREAD_POOL_QUEUE_SIZE, queue.size.toFloat(), tag))
+                }
 
-            it.completedTaskCount?.let { completedTaskCount ->
-                metricList.add(MetricItemVo(DUBBO_THREAD_POOL_COMPLETED_TASK_COUNT, completedTaskCount.toFloat(), tag))
-            }
+                it.completedTaskCount.let { completedTaskCount ->
+                    metricList.add(
+                        MetricItemVo(
+                            DUBBO_THREAD_POOL_COMPLETED_TASK_COUNT,
+                            completedTaskCount.toFloat(),
+                            tag
+                        )
+                    )
+                }
 
         }
     }
